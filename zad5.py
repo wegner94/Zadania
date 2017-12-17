7fccc276e8f5122570ec3870ba1998584047d1d2
wegner94 <34620703+wegner94@users.noreply.github.com>
2017-12-17 14:45:13 +0100

    Update zad5.py

--- zad5.py
+++ zad5.py
@@ -11,21 +11,20 @@ st_1ehz = parser.get_structure('my structure','D:\\Python27\\Programs\\1EHZ.pdb'
 
 atoms_list = []
 at_gen = st_1ehz.get_atoms()
-backbone_list = []
-backbone2_list = []
 
-# Z jakiegoś powodu jednokrotne usunięcie atomów nie kasuje wszystkich. Stąd pętla. 
-for i in range(1,5):
+# Z jakiegoś powodu jednokrotne usunięcie atomów nie eliminuje wszystkich. Stąd pętla while która pracuje tak długo aż pozostaną tylko pożądane atomy
+counter = 0
+next = 'true'
+while next == 'true':
+    counter = counter +1
+    next = 'false'
     for res in st_1ehz[0]['A']:
         for at in res:
             if at.get_name() not in ['P', 'OP1', 'OP2', "O5'", "C5'", "C4'", "O4'", "C1'", "C2'", "O2'", "C3'", "O3'" ]:
-                backbone_list.append(at.name)
+                next = 'true'
                 id = at.id
                 res.detach_child(id)
-    print backbone_list.index('N3')
-    backbone_list = []
-
 
 out = PDBIO()
-out.set_structure(st_backbone)
+out.set_structure(st_1ehz)
 out.save('1EHZ_backbone.pdb')
